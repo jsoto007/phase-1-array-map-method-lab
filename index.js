f913bada7f4b397b5825c1a6a51ca77ef36f078f
Jose Soto <jsoto7087@gmail.com>
2022-04-19 21:30:23 -0400

    commit tree a6231c30e3277895fadba21f2abf504c364ba3e0

--- index.js
+++ index.js
@@ -12,5 +12,32 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  let newString = tutorials.map(function (string) {
+    let words = string.split(" ")
+    let wordsArr = words.map((word) => {
+      return word[0].toUpperCase() + word.substring(1)
+    })
+    console.log(wordsArr)
+    return wordsArr.join(" ")
+  })
+  return newString;
 }
+
+// gets each element of the array.
+//iterates over each element.
+//iterated over a string
+//returns one fine array with the new strings.
+
+// function elementFinder(arrOfStrings) {
+//   const result = arrOfStrings.map((blogPost) => {
+//     return blogPost
+//   });
+//   return result;
+// }
+
+// function stringModifier(string) {
+//   const newString = string.map((stringArr) => {
+//     stringArr[0].toUpperCase() + stringArr.substring(1)
+//   })
+//   return newString;
+// }
